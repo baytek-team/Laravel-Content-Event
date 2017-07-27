c931ba89c80d4c693fb40aa9f10ed0bcb168b8b2
Yvon Viger <yvon@baytek.ca>
2017-07-27 14:26:37 -0400

    Adding admin middleware

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -54,7 +54,7 @@ class RouteServiceProvider extends ServiceProvider
         $routes = 'routes/admin/event.php';
         if(file_exists(base_path($routes))){
             Route::prefix('admin/events')
-                 ->middleware(['web', 'auth'])
+                 ->middleware(['admin'])
                  ->namespace(Controllers::class)
                  ->group(base_path($routes));
         }
